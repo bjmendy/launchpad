4d4a394af89fbad2ccbe5c14743d3854288ca548
Bridjet Mendyuk <bjmendy@Bridjets-MacBook-Pro.local>
2017-08-06 10:31:41 -0500

    update 3

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Areas of update:
 Forks
 Commits
 Pull requests
+
+First, I set up the js file to make an active request to the Github API. I called the API to display to the page. Then, I made three headings and three AJAX calls to the respective API parts I wanted to display; pull requests, commits and forks.
+
--- js/script.js
+++ js/script.js
@@ -2,7 +2,56 @@
 var req = new XMLHttpRequest();
 req.open("POST", "https://api.github.com", true);
 req.onreadystatechange = function () {
-  if (r.readyState != 4 || r.status != 200) return;
-  alert("Success: " + r.responseText);
+  if (req.readyState != 4 || req.status != 200) return;
+  alert("Success: " + req.responseText);
 };
 req.send("its workinnnnnggggg");
+
+//LIST the pull requests from the api
+//LIST the forks from the api
+//LIST the commits from the api
+
+var heading = document.createElement("h1");
+heading.setAttribute("id", "Forks");
+
+$.ajax({
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=052f26926ae9784c2d677ca7bc5dec98',
+  type: "GET",
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+    },
+  fail: function(error){
+	console.log(error)
+  }
+});
+
+var heading2 = document.createElement("h1");
+heading2.setAttribute("id", "Commits");
+
+$.ajax({
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=052f26926ae9784c2d677ca7bc5dec98',
+  type: "GET",
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+    },
+  fail: function(error){
+	console.log(error)
+  }
+});
+
+var heading3 = document.createElement("h1");
+heading3.setAttribute("id", "Pull Requests");
+
+$.ajax({
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=052f26926ae9784c2d677ca7bc5dec98',
+  type: "GET",
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+    },
+  fail: function(error){
+	console.log(error)
+  }
+});
